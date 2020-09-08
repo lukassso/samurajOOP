e1bab55fd818a6ebe560ad8147130d4f213e7ebc
lukasso <lukasz.koladko@gmail.com>
2020-09-08 23:07:56 +0200

    Minesweeper initial part

---
+++ sekcja5-Minesweeper/js/UI.js
@@ -0,0 +1,15 @@
+export class UI {
+  UiSelectors = {
+    board: '[data-board]',
+    cell: '[data-cell]',
+  };
+
+    getElement(selector) {
+    return document.querySelector(selector);
+  }
+  getElements(selector) {
+    return document.querySelectorAll(selector);
+  }
+
+
+}
