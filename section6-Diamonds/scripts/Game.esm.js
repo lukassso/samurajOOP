d5403056ea2c587ee306c12e6ea7a5276fd259ac
lukasso <lukasz.koladko@gmail.com>
2020-09-26 09:14:07 +0200

    added drawGameOnCanvas to Canvas class

--- section6-Diamonds/scripts/Game.esm.js
+++ section6-Diamonds/scripts/Game.esm.js
@@ -1,26 +1,31 @@
 import { canvas } from "./Canvas.esm.js";
 import { Common, VISIBLE_SCREEN } from "./Common.esm.js";
-import {gameLevels} from "./gameLevels.esm.js";
-import {DATA_LOADED_EVENT_NAME, loader} from './Loader.esm.js';
+import { gameLevels } from "./gameLevels.esm.js";
+import { DATA_LOADED_EVENT_NAME, loader } from "./Loader.esm.js";
+
+const gameState = {
+  pointsToWin: 70000,
+  getPlayerPoints: () => 1000,
+  getLeftMovement: () => 30,
+};
 
 class Game extends Common {
   constructor() {
     super();
   }
- 
+
   playLevel(level) {
-    window.removeEventListener(DATA_LOADED_EVENT_NAME, this.playLevel)
-    const levelInfo  = gameLevels[level - 1];
-    this.changeVisibilityScreen(canvas.element, VISIBLE_SCREEN)
+    window.removeEventListener(DATA_LOADED_EVENT_NAME, this.playLevel);
+    const levelInfo = gameLevels[level - 1];
+    this.changeVisibilityScreen(canvas.element, VISIBLE_SCREEN);
     this.animate();
   }
 
   animate() {
     // console.log('Lets game')
-    canvas.drawGameOnCanvas();
-    this.animationFrame = window.requestAnimationFrame(() => this.animate())
+    canvas.drawGameOnCanvas(gameState);
+    this.animationFrame = window.requestAnimationFrame(() => this.animate());
   }
-
 }
 
-export const game = new Game()
+export const game = new Game();
