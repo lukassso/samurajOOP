83ce67a04a31b1622e7a2d213db5c0b0ad78046d
lukasso <lukasz.koladko@gmail.com>
2020-11-18 19:47:35 +0100

    MouseControler

--- section6-Diamonds/scripts/GameState.esm.js
+++ section6-Diamonds/scripts/GameState.esm.js
@@ -4,7 +4,9 @@ export class GameState {
   constructor(level, leftMovement, pointsToWin, diamonds, diamondsSpriteImage) {
     let _leftMovement = leftMovement;
     let _playerScores = 0;
-    let _gameBoard = diamonds.map((diamond) => new Diamond({ ...diamond }));
+    let _gameBoard = diamonds.map((diamond) => new Diamond({ diamondsSpriteImage, ...diamond }));
+    let _isSwaping = false;
+    let _isMoving = false;
     this._pointsToWin = pointsToWin;
     this._level = level;
 
@@ -14,8 +16,14 @@ export class GameState {
 
     this.getPlayerPoints = () => _playerScores;
     this.increasePlayerPoints = (points) => (_playerScores += points);
+    
+    this.getSwaping = () => _isSwaping
+    this.setSwaping = value => _isSwaping = value
+    this.getIsMoving = () => _isMoving;
+    this.setIsMoving = value => _isMoving = value 
 
     this.isPlayerWinner = () => _playerScores >= this._pointsToWin;
+    this.getGameBoard = () => _gameBoard;
   }
 
   get level() {
