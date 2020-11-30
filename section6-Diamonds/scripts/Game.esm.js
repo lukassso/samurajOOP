bbaf6313a349e36a701388caed1e58932cc3a6be
lukasso <lukasz.koladko@gmail.com>
2020-11-30 21:58:31 +0100

    change and moving diamonds

--- section6-Diamonds/scripts/Game.esm.js
+++ section6-Diamonds/scripts/Game.esm.js
@@ -9,6 +9,7 @@ import { mouseController } from "./MouseController.esm.js";
 
 const DIAMONDS_ARRAY_WIDTH = 8;
 const DIAMONDS_ARRAY_HEIGHT = DIAMONDS_ARRAY_WIDTH + 1;
+const SWAPING_SPEED = 8;
 
 // const gameState = {
 //   pointsToWin: 7000,
@@ -32,6 +33,8 @@ class Game extends Common {
   animate() {
     // console.log('Lets game')
     this.handleMouseState();
+    this.handleMouseClick();
+    this.moveDiamonds()
     canvas.drawGameOnCanvas(gameState);
     this.diamond.draw();
     this.animationFrame = window.requestAnimationFrame(() => this.animate());
@@ -98,6 +101,68 @@ class Game extends Common {
     } 
 
     mouseController.clicked = false
+  }
+
+  swapDiamonds() {
+    const firstDiamond = mouseController.firstClick.y * DIAMONDS_ARRAY_WIDTH + mouseController.firstClick.x
+    const secondDiamond = mouseController.secondClick.y * DIAMONDS_ARRAY_WIDTH + mouseController.secondClick.x
+  
+    this.swap(this.gameState.getGameBoard()[firstDiamond], this.gameState.getGameBoard()[secondDiamond])
+
+  }
+
+  moveDiamonds() {
+    this.gameState.setIsMoving(false);
+    this.gameState.getGameBoard().forEach(diamond => {
+      let dx;
+      let dy;
+
+      for(let speedSwap = 0; speedSwap < SWAPING_SPEED; speedSwap++){
+        dx = diamond.x - diamond.row * DIAMOND_SIZE;
+        dy = diamond.y - diamond.column * DIAMOND_SIZE;
+
+        if(dx) {
+          diamond.x = dx/Math.abs(dx);
+        }
+
+        if (dy){
+          diamond.y = dy/Math.abs(dy);
+        }
+      }
+      if(dx || dy ){
+        this.gameState.setIsMoving(true);
+      }
+       
+    })
+  }
+
+  swap(firstDiamond, secondDiamond) {
+    [
+       firstDiamond.kind,
+       firstDiamond.alpha,
+       firstDiamond.match,
+       firstDiamond.x,
+       firstDiamond.y,
+       secondDiamond.kind,
+       secondDiamond.alpha,
+       secondDiamond.match,
+       secondDiamond.x,
+       secondDiamond.y,
+    ] = [
+       secondDiamond.kind,
+       secondDiamond.alpha,
+       secondDiamond.match,
+       secondDiamond.x,
+       secondDiamond.y,
+       firstDiamond.kind,
+       firstDiamond.alpha,
+       firstDiamond.match,
+       firstDiamond.x,
+       firstDiamond.y,
+    ];
+
+    this.gameState.setIsMoving(true);
+
   }
 }
 
