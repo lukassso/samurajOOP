f6434d0a89b0b86938aaa2db31d3470060ec720a
lukasso <lukasz.koladko@gmail.com>
2020-12-09 23:21:06 +0100

    shuffle cards method

--- section7-Blackjack/app/Deck.js
+++ section7-Blackjack/app/Deck.js
@@ -9,4 +9,16 @@ export class Deck {
 
       console.log(this.cards);
   }
+
+  shuffle(){
+    for (let i= this.cards.length -1; i> 0; i--){
+      const j = Math.floor(Math.random() * i);
+      const temp = this.cards[i];
+      this.cards[i] = this.cards[j];
+      this.cards[j] = temp;
+    }
+    return this.cards
+  }
+
+
 }
