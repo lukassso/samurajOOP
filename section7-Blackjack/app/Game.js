c23e0aa4fdd49ad11049f0e016ddf865bf40a672
lukasso <lukasz.koladko@gmail.com>
2020-12-10 22:03:08 +0100

    Game class

--- section7-Blackjack/app/Game.js
+++ section7-Blackjack/app/Game.js
@@ -1,9 +1,9 @@
 import { Deck } from "./Deck.js";
-import {Player} from "./Player.js";
+import { Player } from "./Player.js";
 class Game {
   constructor({ playersCards, dealersCards, player }) {
     this.player = player;
-    this.dealer = new Player('Croupier');
+    this.dealer = new Player("Croupier");
     this.playersCards = playersCards;
     this.dealersCards = dealersCards;
     this.deck = new Deck();
@@ -11,23 +11,21 @@ class Game {
   }
 
   run() {
-
     this.dealCards();
   }
 
-  dealCards(){
-    for(let n=0; n < 2; n++){
+  dealCards() {
+    for (let n = 0; n < 2; n++) {
       let card1 = this.deck.pickOne();
       this.player.hand.addCard(card1);
       this.playersCards.appendChild(card1.render());
-      
+
       let card2 = this.deck.pickOne();
-this.dealer.hand.addCard(card2); 
-this.dealersCards.appendChild(card2.render());
-      console.log(this.player.hand.cards)
+      this.dealer.hand.addCard(card2);
+      this.dealersCards.appendChild(card2.render());
+      console.log(this.player.hand.cards);
     }
   }
-
 }
 
 const player = new Player("Lukas");
