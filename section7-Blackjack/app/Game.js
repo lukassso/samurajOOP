b32b241d1a8a85fd06f3b7cae908fb64a5d501a8
lukasso <lukasz.koladko@gmail.com>
2020-12-15 18:41:53 +0100

    summing up points and game

--- section7-Blackjack/app/Game.js
+++ section7-Blackjack/app/Game.js
@@ -2,9 +2,11 @@ import { Deck } from "./Deck.js";
 import { Player } from "./Player.js";
 import { Table } from "./Table.js";
 class Game {
-  constructor({player, table, hitButton, standButton }) {
+  constructor({player, table, dealerPoints, playerPoints, hitButton, standButton }) {
     this.hitButton = hitButton;
     this.standButton = standButton;
+    this.dealerPoints = dealerPoints;
+    this.playerPoints = playerPoints;
     this.player = player;
     this.dealer = new Player("Croupier");
     this.table = table;
@@ -16,6 +18,7 @@ class Game {
 
   run() {
     this.hitButton.addEventListener('click', (event) => {this.hitCard()})
+    this.standButton.addEventListener('click', (event) => this.dealerPlays())
     this.dealCards();
   }
 
@@ -24,6 +27,8 @@ class Game {
     const card = this.deck.pickOne();
     this.player.hand.addCard(card);
     this.table.showPlayersCard(card);
+  this.playerPoints.innerHTML = this.player.calculatePoints();
+
   }
 
   dealCards() {
@@ -39,7 +44,21 @@ class Game {
       this.table.showDealersCard(card2);
       // console.log(this.player.hand.cards);
     }
+
+  this.playerPoints.innerHTML = this.player.calculatePoints();
+  this.dealerPoints.innerHTML = this.dealer.calculatePoints();
+
   }
+
+dealerPlays() {
+  while (this.dealer.points <= this.player.points && this.dealer.points <= 21 && this.player.points <=21){
+    const card = this.deck.pickOne();
+    this.dealer.hand.addCard(card);
+    this.table.showDealersCard(card);
+    this.dealerPoints.innerHTML = this.dealer.calculatePoints();
+  }
+}
+
 }
 
 const table = new Table( 
@@ -47,9 +66,11 @@ const table = new Table(
   document.getElementById("playersCards")
 )
 
-
 const player = new Player("Lukas");
+
 const game = new Game({
+  playerPoints: document.getElementById('playerPoints'),
+  dealerPoints: document.getElementById('dealerPoints'),
   hitButton: document.getElementById('hit'), 
   standButton: document.getElementById('stand'),
   player,
